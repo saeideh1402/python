06fed1012b2e64534d9efcb545094082eb7d5932
saeideh1402 <140346826+saeideh1402@users.noreply.github.com>
2023-07-28 13:04:19 -0700

    Add files via upload

---
+++ student.py
@@ -0,0 +1,16 @@
+name=(input("enter name:"))
+family=(input("enter family:"))
+a=int(input("enter number1:"))
+b=int(input("enter number2:"))
+c=int(input("enter number3:"))
+d=a+b+c/3
+if d<12 :
+    print (name,("   fail"))
+else:
+
+    
+        if d<=17:
+            print (name,(" normal)"))
+        else:
+            if d>=17:
+                print (name,(" great"))
---
+++ tringle.py
@@ -0,0 +1,7 @@
+a=int(input("enter num1:"))
+b=int(input("enter num2:"))
+c=int(input("enter num3:"))
+if a>b+c:
+    print ("this not a tringle")
+else:
+    print ("this is a tringle")
